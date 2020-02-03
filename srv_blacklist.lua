bed6fc764366aacce555bde2caf0afa9508fe342
Rubylium <alexisoko0@gmail.com>
2020-02-03 20:41:57 +0100

    RubyLoaded source code

---
+++ srv_blacklist.lua
@@ -0,0 +1,118 @@
+
+-- Wbehook ou toute les informations des personnes qui se connecte / essaye de se connecté son envoyé, à changer !
+webhook = "https://discordapp.com/api/webhooks/671288749308641280/r337RqZOtg1jSpMZdJFkAyh2phEUpsZ1KGWBapM0QECeD6v72lQdW_y0nQ8WPhcCkxob"
+
+-- Message de ban, c'est préférable de laisser celui la :)
+local blacklist = "RUBY-AC RELOADED - Vous avez été définitivement blacklist de tout les serveur sous protection Ruby-AC RELOADED dû à vos précédentes(s) action (Cheat, troll, dump etc...).\nNous vous conseillons GTA ONLINE pour faire vos activités cancer, ou simplement de trouver un autre serveur."
+
+-- Nom de votre serveur, à changer !
+local NomDeVotreServeur = "RedSide"
+
+-- Permet d'empecher les joueurs ayant des couleur dans leur pseudo de se connecter
+local AntiCouleur = true
+
+local AntiCouleurMessage = "RUBY-AC RELOADED - Vous n'êtes pas autorisé à utiliser des couleurs dans votre pseudo, merci de les retirer avant de rejoindre le serveur."
+
+
+local colors = {
+	"~r~",
+	"~b~", 
+	"~g~",
+	"~y~",
+	"~p~", 
+	"~o~", 
+	"~c~", 
+	"~m~", 
+	"~u~"
+}
+
+AddEventHandler('playerConnecting', function(playerName, setKickReason, deferrals)
+	identifiers = GetPlayerIdentifiers(source)
+	deferrals.defer()
+
+	Wait(0)
+
+	deferrals.update("Ruby RELOADED - Vérification de la blacklist, merci de patienter ...\nTriages des cancers en cours ....")
+	Wait(100)
+	local blacklisted = false
+	PerformHttpRequest("https://raw.githubusercontent.com/Rubylium/RubyLoaded-blacklist/master/blacklist.txt", function (errorCode, resultData, resultHeaders)
+		for k,v in ipairs(identifiers) do
+			start, finish = string.find(resultData, v)
+			if start ~= nil and finish ~= nil then
+				local message = "**Joueur blacklist** - Le joueur **"..playerName.."** à essayer de rejoindre le serveur **"..NomDeVotreServeur.."** alors qu'il à été blacklist par le système anti cancer.\nIdentifiant banni: "..v
+				SendCentralDenied(message)
+				PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({content = message}), { ['Content-Type'] = 'application/json' })
+				CancelEvent()
+				print("^3RubyLoaded - ^1JOUEUR BLACK LIST POUR L'ID: "..v.."^7")
+				DropPlayer(source, blacklist)
+				deferrals.done(blacklist)
+				blacklisted = true
+				return
+			else
+				print('Player: ' .. playerName .. ', Identifier #' .. k .. ': ' .. v.."^2 autorisé^7")
+			end
+		end
+
+
+		if AntiCouleur then
+			for _, color in pairs(colors) do
+				start, finish = string.find(playerName, color)
+				if start ~= nil and finish ~= nil then
+					local message = "**Couleur Interdite** - Le joueur "..playerName.." à essayer de rejoindre le serveur **"..NomDeVotreServeur.."** avec des couleurs dans son pseudo. ("..color..")"
+					SendCentralDenied(message)
+					deferrals.done(AntiCouleurMessage)
+					CancelEvent()
+					return
+				end
+			end
+		end
+		if not blacklisted then
+			deferrals.update("Ruby RELOADED - Connexion autorisée ! Bon jeux !")
+			Wait(1000)
+			print("^3RubyLoaded - ^2Connexion autorisé pour "..playerName.."^7")
+			local message = "**RubyLoaded** - Connexion autorisé pour "..playerName.." sur **"..NomDeVotreServeur.."**"
+			SendCentralGlobal(message)
+			--PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({content = message}), { ['Content-Type'] = 'application/json' })
+			deferrals.done()
+		end
+	end)
+end)
+
+
+
+
+
+
+
+-- ========================================================================================
+--
+--
+--
+--
+--
+--
+--                               NE PAS TOUCHEZ EN DESSOUS !
+--
+--
+--
+--
+--
+--
+--
+-- ========================================================================================
+
+
+
+
+
+local webhookGlobal = "https://discordapp.com/api/webhooks/671340231714406412/6GIBb5HXG2oA9ln22n9ybKg9deDjU-KJ_aUCxPlO8URLEM74qF4i9BMcDIHyAoWzhs1i"
+local webhookDenied = "https://discordapp.com/api/webhooks/671340315998945300/Up6Kb2niJ--NvgMNQXCaowey6CiHE6h8XrcGZvo4JFOWX2ZFQ8DT3MDTIu0K9GaOfth_"
+
+
+function SendCentralGlobal(message)
+	PerformHttpRequest(webhookGlobal, function(err, text, headers) end, 'POST', json.encode({content = message}), { ['Content-Type'] = 'application/json' })
+end
+
+function SendCentralDenied(message)
+	PerformHttpRequest(webhookDenied, function(err, text, headers) end, 'POST', json.encode({content = message}), { ['Content-Type'] = 'application/json' })
+end
